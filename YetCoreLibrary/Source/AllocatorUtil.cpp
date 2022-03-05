4809ce2336e20f019b2cc8a4a5948565dd3f8110
Alexander Kunsar <ralegen@gmail.com>
2022-03-05 23:28:37 +0300

    Mark `Allocator::raiseError()` as never returning

--- YetCoreLibrary/Source/AllocatorUtil.cpp
+++ YetCoreLibrary/Source/AllocatorUtil.cpp
@@ -5,7 +5,7 @@ namespace {
 	const Ptr FALLBACK_ERROR = Ptr(1);  // TODO: a proper error object
 }
 
-Ptr yet_Allocator_raiseErrorF__U_U_U__PV(EC* context, uintptr_t size, uintptr_t options, uintptr_t placeHint) {
+Ptr yet_Allocator_raiseErrorF__U_U_U__N(EC* context, uintptr_t size, uintptr_t options, uintptr_t placeHint) {
 	// TODO: instantiate a proper error object and fill debug data from provided context and arguments
 	//auto place = Allocator::allocate(errorSize, Allocator::Options::CAN_USE_EMERGENCY_POOL);
 	return FALLBACK_ERROR;
