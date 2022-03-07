cb8e93f7aecab32782f0946ac782511912399a75
Alexander Kunsar <ralegen@gmail.com>
2022-03-07 18:07:51 +0300

    Define aliases for primitive types

---
+++ YetCoreLibrary/Include/Primitives.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <cstdint>
+
+/// <summary>
+/// Unsigned integer with the size of 8 bits.
+/// </summary>
+using UInt8 = uint8_t;
+
+/// <summary>
+/// Unsigned integer with the size of 16 bits.
+/// </summary>
+using UInt16 = uint16_t;
+
+/// <summary>
+/// Unsigned integer with the size of 32 bits.
+/// </summary>
+using UInt32 = uint32_t;
+
+/// <summary>
+/// Unsigned integer with the size of 64 bits.
+/// </summary>
+using UInt64 = uint64_t;
+
+/// <summary>
+/// Unsigned integer with the size of machine word, e.g., 64 bits for 64-bit machine.
+/// </summary>
+using UInt = uintptr_t;
+
+/// <summary>
+/// Signed integer with the size of 8 bits.
+/// </summary>
+using Int8 = int8_t;
+
+/// <summary>
+/// Signed integer with the size of 16 bits.
+/// </summary>
+using Int16 = int16_t;
+
+/// <summary>
+/// Signed integer with the size of 32 bits.
+/// </summary>
+using Int32 = int32_t;
+
+/// <summary>
+/// Signed integer with the size of 64 bits.
+/// </summary>
+using Int64 = int64_t;
+
+/// <summary>
+/// Signed integer with the size of machine word, e.g., 64 bits for 64-bit machine.
+/// </summary>
+using Int = intptr_t;
